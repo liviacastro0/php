72050ac2369dcb6901c646ec57401a988348e3f2
livia-castro <145464728+liviacastro0@users.noreply.github.com>
2023-12-19 10:31:37 -0300

    Add files via upload

---
+++ aula01/exp07.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+   echo "<h2> livinha </h2>";
+   echo " sou linda  <br>";
+   echo "eu estou aprendendo PHP! <br>";
+   print " isto "." string ". "foi " . "feito ". " com multiplos parametros ";
+   
+   $txt1 = "amoooooo PHP";
+$txt2 = "W3Schools.com ";
+$x = 5;
+$y = 4;
+   print "<h2>" . $txt1. "</h2>";
+   print " Study php at " . $txt2 . "<br>";
+   print $x + $y;
+   ?>
+</body>
+</html>
