63873df1f1ae13c2bfb6036f1cf938696a923f36
livia-castro <145464728+liviacastro0@users.noreply.github.com>
2024-04-30 11:38:42 -0300

    Add files via upload

---
+++ php/aula-30-04/exercicio1.php
@@ -0,0 +1,23 @@
+<?php 
+$aluno ["nome"] = array ("fabio", "livia", "duda");
+$aluno ["idade"] = array ("30", "40", "50");
+$aluno ["ende"] = array ("rua b", "rua c", "rua d");
+$aluno ["cidade"] = "fabianix";
+$aluno ["estado"] = "fabioestate";
+$aluno ["cpf"] = array ("000.000.000-00", "000.000.000-01", "000.000.000-02");
+echo $aluno["nome"][0] . "<br>";
+echo $aluno["idade"][0] . "<br>";
+echo $aluno["ende"][0] . "<br>";
+echo $aluno["cidade"] . "<br>";
+echo $aluno["estado"] . "<br>";
+echo $aluno["cpf"][0] . "<br>";
+
+ 
+$professor ["nome"] = "fabio";
+$professor ["idade"] = "49";
+$professor ["end"] = "alameda boa sorte";
+$professor ["cidade"] = "fabio";
+$professor ["estado"] = "fabio";
+$professor ["cpf"] = "000.000.000-00";
+
+?>
---
+++ php/aula-30-04/index3.php
@@ -0,0 +1,17 @@
+<?php
+$var= array (1,2,3,4,5);
+echo "$var[0]";
+echo "$var[1]";
+echo "$var[2]";
+echo "$var[3]";
+echo "$var[4]";
+
+$var2 = array(0 => 5, 6 => 8, 9 => 15);
+
+echo"<br>";
+
+foreach ($var2 as $teste1) {
+    echo "$teste1 <br>";
+};
+?>
+
